e150f5f13143eea5095e8e64356ce60dba403049
Onni Ovaskainen <onni.ovaskainen@helsinki.fi>
2020-03-22 20:14:16 +0200

    Viikko2 commit

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,34 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void SaldoAlussaOikein() {
+        assertEquals("10", String.valueOf(kortti.saldo()));
+    }
+    @Test
+    public void kortilleVoiLadataRahaa() {
+        kortti.lataaRahaa(25);
+        assertEquals("saldo: 0.35", kortti.toString());
+    }
+    
+    @Test
+    public void saldoVaheneeRahaaTarpeeksi(){
+        kortti.otaRahaa(5);
+        assertEquals("saldo: 0.5", kortti.toString());
+    }
+    @Test
+    public void saldoEiVaheneRahaaEiTarpeeksi(){
+        kortti.otaRahaa(15);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    @Test
+    public void otaRahaaPalauttaaTrueRiittavastiRahaa(){
+        assertEquals(true, kortti.otaRahaa(5));
+    }
+    @Test
+    public void otaRahaaPalauttaaFalseEiRiittavastiRahaa(){
+        assertEquals(false, kortti.otaRahaa(25));
+    }
+    
 }
