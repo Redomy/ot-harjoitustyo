06446ad7ddc267c27a6a283b5ce8cbc7c20657f9
Onni <noreply-onni.ovaskainen@helsinki.fi>
2020-04-07 12:59:33 +0300

    update

---
+++ Tile.java
@@ -0,0 +1,65 @@
+
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import static javafx.scene.paint.Color.GRAY;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+
+public class Tile extends StackPane{
+    private int x;
+    private int y;
+    private boolean hasBomb;
+    private Rectangle nelio = new Rectangle(Minesweeper.getTileSize(), Minesweeper.getTileSize());
+    private Text text = new Text();
+    private boolean revealed;
+    
+    public int getX(){
+        return this.x;
+    }
+    public int getY(){
+        return this.y;
+    }
+    public boolean bombStatus(){
+        return this.hasBomb;
+    }
+    public void modifyText(String newText){
+        this.text.setText(newText);
+    }
+    public Tile(int x, int y, boolean hasBomb){
+        this.revealed = false;
+        this.x = x;
+        this.y =y;
+        this.hasBomb = hasBomb;
+        nelio.setStroke(Color.CRIMSON);
+        nelio.setFill(Color.LIGHTSEAGREEN);
+        if(this.hasBomb == true){
+            text.setText("X");
+        }else{
+            text.setText("");
+        }
+        text.setFont(Font.font(16));
+        getChildren().add(text);
+        getChildren().add(nelio);
+        
+        setTranslateY(y * Minesweeper.getTileSize());
+        setTranslateX(x * Minesweeper.getTileSize());
+        setOnMouseClicked(event -> reveal());
+    }
+    public void reveal(){
+        if(revealed){
+            return;
+        }
+        if(this.bombStatus()){
+            System.exit(0);
+        }
+        revealed = true;
+        nelio.setFill(null);
+        if(this.text.getText().isEmpty()){
+            Minesweeper.getNeighbours(this).forEach(t -> t.reveal());
+        }
+    }
+    
+}
