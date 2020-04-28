48ddc839fb56046b727819cbb59052c6214fa453
Onni Ovaskainen <onni.ovaskainen@helsinki.fi>
2020-04-28 16:32:34 +0300

    highscoret

--- MineSweeper/src/main/java/Minesweeper/ui/MinesweeperUI.java
+++ MineSweeper/src/main/java/Minesweeper/ui/MinesweeperUI.java
@@ -4,6 +4,11 @@ package Minesweeper.ui;
 import com.mycompany.minesweeper.DatabaseUser;
 import com.mycompany.minesweeper.Minesweeper;
 import com.mycompany.minesweeper.Tile;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.animation.AnimationTimer;
 import javafx.geometry.Insets;
 import javafx.application.Application;
@@ -19,12 +24,14 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MinesweeperUI extends Application {
+    private DatabaseUser db;
     private static Pane alusta = new Pane();
     private static Text teksti = new Text("");
 //    private static Scene scenePlay = new Scene(alusta);
 //    private Scene scene = new Scene(alusta);
+//    private BorderPane scoreMenu = new BorderPane();
+//    ArrayList<Text> texts = new ArrayList<>();
     public static void addTile(Tile tile){
-
         alusta.getChildren().add(tile);
     }
     public static Pane startGame(){
@@ -34,20 +41,50 @@ public class MinesweeperUI extends Application {
         Minesweeper.setupGame();
         return alusta;
     }
+    public BorderPane makeScorePane() throws ClassNotFoundException, SQLException{
+        BorderPane root = new BorderPane();
+        root.setPrefHeight(Minesweeper.getHeight());
+        root.setPrefWidth(Minesweeper.getWidth());
+        VBox scorelist = new VBox();
+        Text text1 = new Text();
+        Text text2 = new Text();
+        Text text3 = new Text();
+        Text text4 = new Text();
+        Text text5 = new Text();
+        ArrayList<Text> texts = new ArrayList<>();
+        texts.add(text1);
+        texts.add(text2);
+        texts.add(text3);
+        texts.add(text4);
+        texts.add(text5);
+        ArrayList<String> results = db.getHighScores();
+        for(int i = 0; i < results.size(); i++){
+            texts.get(i).setText(results.get(i));
+        }
+        scorelist.getChildren().addAll(text1, text2, text3, text4, text5);
+        root.setCenter(scorelist);
+        return root;
+    }
     
     @Override
     public void start(Stage stage) throws ClassNotFoundException{
         
-        alusta = startGame();
-        Scene scenePlay = new Scene(alusta);
-        DatabaseUser db = new DatabaseUser();
+//        alusta = startGame();
+//        Scene scenePlay = new Scene(alusta);
+        db = new DatabaseUser();
         db.makeDatabase();
+        
         
         BorderPane asettelu = new BorderPane();
         VBox menu = new VBox();
         menu.setSpacing(20);
+        
+        
         Button play = new Button("Play");
+        Button scores = new Button("Highscores");
         Button quit = new Button("Quit");
+        
+        
         play.setOnAction(event -> {
                         Minesweeper.changeStatus(true);
                         Minesweeper.refreshScore();
@@ -55,19 +92,35 @@ public class MinesweeperUI extends Application {
                         Scene scenePlay2 = new Scene(alusta);
                         stage.setScene(scenePlay2);
                         });
+        scores.setOnAction(event -> {
+            try {
+                BorderPane root = makeScorePane();
+                Scene scoreScene = new Scene(root);
+                stage.setScene(scoreScene);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(MinesweeperUI.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println("Virhe");
+            } catch (SQLException ex) {
+                Logger.getLogger(MinesweeperUI.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            });
+        
         quit.setOnAction(event -> System.exit(0));
-        menu.getChildren().addAll(teksti, play, quit);
+        menu.getChildren().addAll(teksti, play, scores, quit);
         asettelu.setTop(menu);
         asettelu.setPrefSize(Minesweeper.getWidth(), Minesweeper.getHeight());
         asettelu.setPadding(new Insets(20, 20, 20, 20));
         
         Scene sceneMenu = new Scene(asettelu);
         
+        
+        
         new AnimationTimer(){
             public void handle(long nykyhetki){
                 if(Minesweeper.getStatus() == false){
                     teksti.setText("You got " + Minesweeper.getScore() + " points!");
                     stage.setScene(sceneMenu);
+                    Minesweeper.changeStatus(true);
                 }
             }
         }.start();
--- MineSweeper/src/main/java/com/mycompany/minesweeper/DatabaseUser.java
+++ MineSweeper/src/main/java/com/mycompany/minesweeper/DatabaseUser.java
@@ -2,9 +2,11 @@
 package com.mycompany.minesweeper;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DatabaseUser {
     private Connection connection;
+    
     public void makeDatabase() throws ClassNotFoundException{
         String url = "jdbc:sqlite:Scores.db";
         try{
@@ -19,7 +21,38 @@ public class DatabaseUser {
             System.out.println(e.getMessage());
         }
     }
-    public void WriteDatabase(String playerName){
+    public void WriteDatabase(String playerName, int score) throws ClassNotFoundException{
+        String url = "jdbc:sqlite:Scores.db";
         
+        try{
+            Class.forName("org.sqlite.JDBC");
+            this.connection = DriverManager.getConnection(url);
+            String insert = "INSERT INTO Scores (player, score) VALUES (?, ?);";
+            PreparedStatement prStatement = this.connection.prepareStatement(insert);
+            prStatement.setString(1, playerName);
+            prStatement.setInt(2, score);
+            prStatement.execute();
+            this.connection.close();
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+    public ArrayList getHighScores() throws ClassNotFoundException{
+        String url = "jdbc:sqlite:Scores.db";
+        ArrayList<String> results = new ArrayList<>();
+        try{
+            Class.forName("org.sqlite.JDBC");
+            this.connection = DriverManager.getConnection(url);
+            String getScores = "SELECT player, score FROM Scores ORDER BY score DESC LIMIT 5;";
+            Statement statement = this.connection.createStatement();
+            ResultSet resultset = statement.executeQuery(getScores);
+            while(resultset.next()){
+                results.add(resultset.getString("player") + ": " + String.valueOf(resultset.getInt("score")));
+            }
+            this.connection.close();
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return results;
     }
 }
